acf9453abe99b60089c6d40cfe26517711396bc4
Tirth Prajapati <tirthprajapati26@gmail.com>
2024-08-01 17:43:18 +0530

    feat(stringCalc.js) : case added - do multiplication if delimiter is '-'

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -3,23 +3,31 @@ const add = (numbers) => {
     if(numbers === ""){
         return 0;
     }
+    let doMult = false;
     let delimiter = ","; // initally seperated by , 
     if (numbers.startsWith('//')) {
         const delimiterEndIndex = numbers.indexOf('\n');
         delimiter = new RegExp(numbers.substring(2, delimiterEndIndex)); // setting delimiter based on given delimiter
         console.log(delimiter)
+        if(delimiter == "/-/"){
+            doMult=true;
+        }
+        console.log(delimiter)
         numbers = numbers.substring(delimiterEndIndex + 1);
-      } else {
+    } else {
         numbers = numbers.replace(/[\n,]/g, ',');
-      }
+    }
     console.log(numbers)
     const tokenizedNums = numbers.split(delimiter).map(Number);
     let sum = 0;
+    let mult = 1;
     console.log(tokenizedNums)
     const negativeNumberList = []
     tokenizedNums.forEach(element => {
         if(element<0){
             negativeNumberList.push(element)
+        }else if(doMult){
+            mult *= element;
         }else{
             sum += element;
         }
@@ -28,8 +36,12 @@ const add = (numbers) => {
         let errorMessage = `negative numbers not allowed: ${negativeNumberList.join(',')}`;
         throw new Error(errorMessage);
     }
+    if(doMult){
+        return mult
+    }
     return sum;
 }
+
 module.exports = {
  add
 }
